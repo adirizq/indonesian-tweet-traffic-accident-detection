Fixing label preprocessing and softmax on finetune

models/finetune.py
@@ -67,7 +67,7 @@ class Finetune(pl.LightningModule):
         loss = outputs.loss
 
         true = label_batch.to(torch.device("cpu"))
-        pred = torch.argmax(F.softmax(outputs.logits), dim=1).to(torch.device("cpu"))
+        pred = torch.argmax(F.softmax(outputs.logits, dim=1), dim=1).to(torch.device("cpu"))
 
         cls_report = classification_report(true, pred, output_dict=True)
 
@@ -82,6 +82,6 @@ class Finetune(pl.LightningModule):
         input_ids, attention_mask = batch
         outputs = self.model(input_ids, attention_mask)
 
-        pred = torch.argmax(F.softmax(outputs.logits), dim=1).to(torch.device("cpu"))
+        pred = torch.argmax(F.softmax(outputs.logits, dim=1), dim=1).to(torch.device("cpu"))
 
         return pred[0]

utils/preprocessor.py
@@ -54,9 +54,6 @@ class TwitterDataModule(pl.LightningDataModule):
         x_input_ids, x_attention_mask, y = [], [], []
         for (text, label) in tqdm(dataset.values.tolist()):
 
-            encoded_label = [0, 0]
-            encoded_label[label] = 1
-
             encoded_text = self.tokenizer(text=text,
                                           max_length=self.max_length,
                                           padding="max_length",
@@ -64,7 +61,7 @@ class TwitterDataModule(pl.LightningDataModule):
 
             x_input_ids.append(encoded_text['input_ids'])
             x_attention_mask.append(encoded_text['attention_mask'])
-            y.append(encoded_label)
+            y.append(label)
 
         x_input_ids = torch.tensor(x_input_ids)
         x_attention_mask = torch.tensor(x_attention_mask)