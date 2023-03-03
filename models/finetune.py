a56622f69b89714239036aa64b53a74b06e1c31d
Rizky Adi <72495089+adirizq@users.noreply.github.com>
2023-03-03 15:44:12 +0700

    update

--- models/finetune.py
+++ models/finetune.py
@@ -38,7 +38,7 @@ class Finetune(pl.LightningModule):
         return loss, true, pred
 
     def validation_epoch_end(self, validation_step_outputs):
-        print(validation_step_outputs[0])
+        print(validation_step_outputs[:, 1])
         sys.exit()
 
         all_outputs = torch.stack(validation_step_outputs)
