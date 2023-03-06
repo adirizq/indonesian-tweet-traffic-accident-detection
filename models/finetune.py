1d1a4121996c1c2bb633314dbef44b693a9a5d30
adirizq <rizkyzadadi@gmail.com>
2023-03-06 22:28:59 +0700

    update

--- models/finetune.py
+++ models/finetune.py
@@ -126,8 +126,9 @@ class Finetune(pl.LightningModule):
     def _shared_eval_step(self, batch, batch_idx):
         input_ids, attention_mask, targets = batch
         outputs = self(input_ids=input_ids, attention_mask=attention_mask)
-        print(outputs.size())
-        outputs = torch.squeeze(outputs)
+
+        if len(outputs.size()) >= 2:
+            outputs = torch.squeeze(outputs)
 
         print(outputs.size())
         print(targets.size())
